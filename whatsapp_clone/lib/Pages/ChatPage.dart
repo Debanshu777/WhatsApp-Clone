1ce5a33c4c55874378afcc7510948d6b1c57c36d
Debanshu Datta <debanshudatta123@gmail.com>
2021-03-28 21:49:45 +0530

    Added Dynamic List of items,Model class, Individual Chat page

--- whatsapp_clone/lib/Pages/ChatPage.dart
+++ whatsapp_clone/lib/Pages/ChatPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/CustomUI/CustomCard.dart';
+import 'package:whatsapp_clone/Model/ChatModel.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -6,12 +8,54 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatModel> chats = [
+    ChatModel(
+        name: "Anu",
+        icon: "person.svg",
+        isGroup: false,
+        time: "4:00 am",
+        currentMessage: "Sogaye lagta hai"),
+    ChatModel(
+        name: "Sakshi",
+        icon: "person.svg",
+        isGroup: false,
+        time: "4:00 am",
+        currentMessage: "Party karooo"),
+    ChatModel(
+        name: "Lassan",
+        icon: "groups.svg",
+        isGroup: true,
+        time: "10:00 pm",
+        currentMessage: "Party karooo"),
+    ChatModel(
+        name: "Debanshu",
+        icon: "person.svg",
+        isGroup: false,
+        time: "4:00 am",
+        currentMessage: "Enjoy"),
+    ChatModel(
+        name: "D Mutt",
+        icon: "person.svg",
+        isGroup: false,
+        time: "4:00 am",
+        currentMessage: "Kaisa Hai bhai tum"),
+    ChatModel(
+        name: "Friends",
+        icon: "groups.svg",
+        isGroup: true,
+        time: "4:00 am",
+        currentMessage: "Helloooooo"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.chat),
+      ),
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index) => CustomCard(chatModel: chats[index]),
       ),
     );
   }
---
+++ whatsapp_clone/lib/Screens/IndividualChat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IndividualChat extends StatefulWidget {
+  @override
+  _IndividualChatState createState() => _IndividualChatState();
+}
+
+class _IndividualChatState extends State<IndividualChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
