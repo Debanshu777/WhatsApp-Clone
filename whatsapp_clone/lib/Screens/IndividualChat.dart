6590031f13c111de0086594d729ac94ebbfe158c
Debanshu Datta <debanshudatta123@gmail.com>
2021-03-29 00:48:41 +0530

    Added Attachment Modal

--- whatsapp_clone/lib/Screens/IndividualChat.dart
+++ whatsapp_clone/lib/Screens/IndividualChat.dart
@@ -153,7 +153,14 @@ class _IndividualChatState extends State<IndividualChat> {
                                     children: [
                                       IconButton(
                                         icon: Icon(Icons.attach_file),
-                                        onPressed: () {},
+                                        onPressed: () {
+                                          showModalBottomSheet(
+                                              backgroundColor:
+                                                  Colors.transparent,
+                                              context: context,
+                                              builder: (builder) =>
+                                                  bottomSheet());
+                                        },
                                       ),
                                       IconButton(
                                         icon: Icon(Icons.camera_alt),
@@ -213,6 +220,73 @@ class _IndividualChatState extends State<IndividualChat> {
           _controller.text = _controller.text + emoji.emoji;
         });
       },
+    );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+        height: 278,
+        width: MediaQuery.of(context).size.width,
+        child: Card(
+          margin: EdgeInsets.all(18),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    bottomSheetIconCreation(
+                        Icons.insert_drive_file, Colors.indigo, "Document"),
+                    SizedBox(width: 40),
+                    bottomSheetIconCreation(
+                        Icons.camera_alt, Colors.pink, "Camera"),
+                    SizedBox(width: 40),
+                    bottomSheetIconCreation(
+                        Icons.insert_photo, Colors.purple, "Gallery"),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    bottomSheetIconCreation(
+                        Icons.headset, Colors.orange, "Audio"),
+                    SizedBox(width: 40),
+                    bottomSheetIconCreation(
+                        Icons.location_pin, Colors.teal, "Location"),
+                    SizedBox(width: 40),
+                    bottomSheetIconCreation(
+                        Icons.person, Colors.blue, "Contact"),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget bottomSheetIconCreation(IconData icon, Color color, String text) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          CircleAvatar(
+            backgroundColor: color,
+            radius: 30,
+            child: Icon(
+              icon,
+              size: 29,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            text,
+            style: TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
     );
   }
 }
